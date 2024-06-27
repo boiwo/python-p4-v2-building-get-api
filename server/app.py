488bfb99ef80b2049c050fab5b36d5fecc11a2b0
Brian <brianhilsden@gmail.com>
2024-06-27 18:02:25 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -21,6 +21,41 @@ def index():
 
 # start building your API here
 
+@app.route("/games")
+def games():
+    games_dict = [game.to_dict() for game in Game.query.order_by(Game.title).all()]
+    response = make_response(
+        games_dict,200, {"Content-Type":"application/json"})
+    return response
+
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    if game:
+        body = game.to_dict()
+        status = 200
+    else:
+        body = {"message" : f"Game id {id} does not exist"}
+        status = 404
+
+    response = make_response(body,status,{"Content-Type":"application/json"})
+    return response
+
+@app.route("/games/users/<int:id>")
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    if game:
+        body = [user.to_dict(rules = ("-reviews",) ) for user in game.users]
+        status = 200
+    else:
+        body = {"message": f"Game {id} not found"}
+        status = 404
+    
+    response = make_response(body,status,{"Content-Type":"application/json"})
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
